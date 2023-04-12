371758c585793c86dfb713976342701b53901dfc
Rectangleuser <127643482+Rectangleuser@users.noreply.github.com>
2023-04-12 12:27:21 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-sum = 0
-ind = 1
+#include<iostream>
 
-while (True):
-    num = input("Enter a number: ")
-    if str(num) != 'q':
-        sum = sum + int(num)
-        print(num, "\t\t\t\t\t[" + str(ind) + "]")
-        ind = int(ind)+1
-    else:
-        print("Ans","=",sum)
+using namespace std;
+
+int main()
+{
+    int I;
+    while (true){
+        cout<<"\nGuess the number: ";
+        cin>>I;
+        if (I == 5){
+            cout<<"\nYou won!";
+        } else {
+            cout<<"You Lose!!!!!";
+        }
+    }
+}
